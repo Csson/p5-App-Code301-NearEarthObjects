913b354e9accba46bb32a7aeab9c1b1d2cb77fea
Erik Carlsson <info@code301.com>
2016-01-08 20:51:31 +0000

    Init

---
+++ lib/App/Code301/NearEarthObjects.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package App::Code301::NearEarthObjects;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use App::Code301::NearEarthObjects;
+
+=head1 DESCRIPTION
+
+App::Code301::NearEarthObjects is ...
+
+=head1 SEE ALSO
+
+=cut
